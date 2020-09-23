8ce22c48be0d6e6f465a60316993ca0552f49507
410902054 <71751283+410902054@users.noreply.github.com>
2020-09-23 15:04:49 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_forever():
+    basic.show_number(0)
+    basic.pause(200)
+    basic.clear_screen()
+    basic.pause(200)
+    basic.show_string("tortoise")
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showNumber(0)
+    basic.pause(200)
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showString("tortoise")
+})
